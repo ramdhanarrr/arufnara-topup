859c6d482915c38b1c81326834b903ae77823761
uazmi91 <uazmi910@gmal.com>
2025-06-09 08:34:30 +0700

    update style admin

--- arufnara-fe/src/components/Admin/Admin.jsx
+++ arufnara-fe/src/components/Admin/Admin.jsx
@@ -4,10 +4,129 @@ import React from 'react';
 const Admin = ({ children }) => {
   return (
     <div>
-      <header className="bg-gray-800 text-white p-4">Admin Control</header>
+
+       {/* Header */}
+
+      <header className="bg-gray-800 text-white p-4 flex items-center justify-start md:justify-center">
+        <div className="flex items-center">
+          <img className="w-7 h-7 md:w-10 md:h-10 mr-2 rounded-md overflow-hidden" src="/logo.png"/>
+          <span className="text-lg font-semibold"> Admin Control </span>
+        </div>
+        
+        <div className='flex justify-center items-center flex-1 mx-4'>
+          <div className='bg-white rounded flex items-center w-full max-w-xl mr-4 p-2 shadow-sm border border-gray-200'>
+            <button className='outline-none focus:outline-none'>
+              <svg className='w-5 text-gray-600 h-5 cursor-pointer' fill="none" strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} stroke="currentColor" viewBox="0 0 24 24">
+                <path d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"/>
+              </svg>
+            </button>
+            <input type="search" name="" id="" placeholder="Search" className='w-full pl-3 text-sm text-black outline-none focus:outline-none bg-transparent'/>
+          </div>
+        </div>
+        <ul className="flex items-center">
+          <li>
+            <div className='block w-px h-6'></div>
+          </li>
+          <li>
+            <a href="" className='flex items-center mr-4 hover:text-blue-100'>
+              <span className='inline-flex mr-1'>
+                <svg className='w-5 h-5' fill='none' stroke='currentColor' viewBox='0 0 24 24' xmlns='http://www.w3.org/2000/svg'/>
+                <path strokeLinecap='round' strokeLinejoin='round' strokeWidth={2} d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1"/>
+              </span>
+              Logout
+            </a>
+          </li>
+        </ul>
+      </header>
+
+      {/* sidebar */}
+
       <div className="flex">
         <aside className="w-64 bg-gray-200 min-h-screen p-4">
-          <nav>
+          <div className='fixed flex flex-col top-14 left-0 w-14 hover:w-64 md:w-64'> 
+            <div className='overflow-y-auto overflow-x-hidden flex flex-col justify-between flex-grow'>
+              <ul className='flex flex-col py-4 space-y-1'>
+                <li className='px-5 hidden md:block'>
+                  <div className='flex flex-row items-center h-8'>
+                    <div className='text-sm font-light tracking-wide text-gray-400 uppercase'>
+                      Main
+                    </div>
+                  </div>
+                </li>
+                <li>
+                  <a href="/admin" className='relative flex flex-row items-center h-11 focus:outline-none'>
+                    <span className='inline-flex justify-center items-center ml-4'>
+                      <svg className='w-5 h-5' fill='none' stroke='currentColor' viewBox='0 0 24 24' xmlns='http://www.w3.org/2000/svg'>
+                        <path strokeLinecap='round' strokeLinejoin='round' strokeWidth={2} d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"/>
+                      </svg>
+                    </span>
+                    <span className='ml-2 text-sm tracking-wide truncate'> Dashboard </span>
+                  </a>
+                </li>
+                <li>
+                  <a href="/admin/topup" className='relative flex flex-row items-center h-11 focus:outline-none'>
+                    <span className='inline-flex justify-center items-center ml-4'>
+                      <svg className='w-5 h-5' fill='none' stroke='currentColor' viewBox='0 0 24 24' xmlns='http://www.w3.org/2000/svg'>
+                        <path strokeLinecap='round' strokeLinejoin='round' strokeWidth={2} d="M20 13V6a2 2 0 00-2-2H6a2 2 0 00-2 2v7m16 0v5a2 2 0 01-2 2H6a2 2 0 01-2-2v-5m16 0h-2.586a1 1 0 00-.707.293l-2.414 2.414a1 1 0 01-.707.293h-3.172a1 1 0 01-.707-.293l-2.414-2.414A1 1 0 006.586 13H4"/>
+                      </svg>
+                    </span>
+                    <span className='ml-2 text-sm tracking-wide truncate'>TopUp</span>
+                  </a>
+                </li>
+                <li>
+                  <a href="/admin/order" className='relative flex flex-row items-center h-11 focus:outline-none'>
+                    <span className='inline-flex justify-center items-center ml-4'>
+                      <svg className='w-5 h-5' fill='none' stroke='currentColor' viewBox='0 0 24 24' xmlns='http://www.w3.org/2000/svg'>
+                        <path strokeLinecap='round' strokeLinejoin='round' strokeWidth={2} d="M3 7h18M3 12h18M3 17h18" />
+                      </svg>
+                    </span>
+                    <span className='ml-2 text-sm tracking-wide truncate'>Order</span>
+                  </a>
+                </li>
+                <li>
+                  <a href="/admin/payment" className='relative flex flex-row items-center h-11 focus:outline-none'>
+                    <span className='inline-flex justify-center items-center ml-4'>
+                      <svg className='w-5 h-5' fill='none' stroke='currentColor' viewBox='0 0 24 24' xmlns='http://www.w3.org/2000/svg'>
+                        <path strokeLinecap='round' strokeLinejoin='round' strokeWidth={2} d="M17 9V7a2 2 0 00-2-2H5a2 2 0 00-2 2v2m14 0v2a2 2 0 01-2 2H5a2 2 0 01-2-2V9m16 0h2a2 2 0 012 2v4a2 2 0 01-2 2h-2"/>
+                      </svg>
+                    </span>
+                    <span className='ml-2 text-sm tracking-wide truncate'>Payment</span>
+                  </a>
+                </li>
+                <li>
+                  <a href="/admin/user" className='relative flex flex-row items-center h-11 focus:outline-none'>
+                    <span className='inline-flex justify-center items-center ml-4'>
+                      <svg className='w-5 h-5' fill='none' stroke='currentColor' viewBox='0 0 24 24' xmlns='http://www.w3.org/2000/svg'>
+                        <path strokeLinecap='round' strokeLinejoin='round' strokeWidth={2} d="M5.121 17.804A10.974 10.974 0 0112 15c2.5 0 4.847.797 6.879 2.15M15 11a3 3 0 11-6 0 3 3 0 016 0z"/>
+                      </svg>
+                    </span>
+                    <span className='ml-2 text-sm tracking-wide truncate'>User</span>
+                  </a>
+                </li>
+                <li>
+                  <a href="/admin/database" className='relative flex flex-row items-center h-11 focus:outline-none'>
+                    <span className='inline-flex justify-center items-center ml-4'>
+                      <svg className='w-5 h-5' fill='none' stroke='currentColor' viewBox='0 0 24 24' xmlns='http://www.w3.org/2000/svg'>
+                        <path strokeLinecap='round' strokeLinejoin='round' strokeWidth={2} d="M12 3c-4.418 0-8 1.79-8 4v10c0 2.21 3.582 4 8 4s8-1.79 8-4V7c0-2.21-3.582-4-8-4z" />
+                      </svg>
+                    </span>
+                    <span className='ml-2 text-sm tracking-wide truncate'>Database</span>
+                  </a>
+                </li>
+                <li>
+                  <a href="/admin/point" className='relative flex flex-row items-center h-11 focus:outline-none'>
+                    <span className='inline-flex justify-center items-center ml-4'>
+                      <svg className='w-5 h-5' fill='none' stroke='currentColor' viewBox='0 0 24 24' xmlns='http://www.w3.org/2000/svg'>
+                        <path strokeLinecap='round' strokeLinejoin='round' strokeWidth={2} d="M12 6v6l4 2"/>
+                      </svg>
+                    </span>
+                    <span className='ml-2 text-sm tracking-wide truncate'>Point</span>
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+          {/* <nav>
             <ul className="space-y-2">
               <li><a href="/admin">Dashboard</a></li>
               <li><a href="/admin/database">DataBase</a></li>
@@ -17,7 +136,7 @@ const Admin = ({ children }) => {
               <li><a href="/admin/user">User</a></li>
               <li><a href="/admin/order">Order</a></li>
             </ul>
-          </nav>
+          </nav> */}
         </aside>
         <main className="flex-1 p-6">{children}</main>
       </div>
